d4ea2f7577bcdfaeadddd7eec6e737af520868ab
norphiz <103674674+norphiz@users.noreply.github.com>
2023-10-16 17:42:14 +0000

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ vim.o.ruler=0
 vim.o.number=1
 vim.o.tabstop=4
 vim.o.showcmd=0
+vim.o.hlsearch=0
 vim.o.showmode=0
 vim.o.swapfile=0
 vim.o.expandtab=1
@@ -16,4 +17,8 @@ vim.o.clipboard="unnamedplus"
 
 require "hl"
 
-vim.api.nvim_create_autocmd("FileType", {pattern="*", callback=function()vim.opt.formatoptions:remove{"c","r","o"}end})
+vim.api.nvim_create_autocmd("FileType", {
+    pattern="*", callback=function()
+        vim.opt.formatoptions:remove{"c", "r", "o"}
+    end
+})
