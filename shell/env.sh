835dcaf447c6a720176756477a15af96f2f9ddd8
norphiz <103674674+norphiz@users.noreply.github.com>
2024-05-21 13:48:47 -0300

    Create env.sh

---
+++ shell/env.sh
@@ -0,0 +1,10 @@
+export LESS='-R'
+export EDITOR='nvim'
+export LESSHISTFILE='-'
+export BAT_THEME='Dracula'
+export MANPAGER='nvim +Man!'
+export SUDO_PROMPT='Password:'
+export NVIM_LOG_FILE='/dev/null'
+export ZDOTDIR="$HOME/.config/zsh"
+export XDG_CACHE_HOME="$HOME/.local/cache"
+export FREETYPE_PROPERTIES='cff:no-stem-darkening=0 autofitter:no-stem-darkening=0'
