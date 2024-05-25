15888804faa1fcf8ae2dadc592dbf3bacb193d6f
norphiz <103674674+norphiz@users.noreply.github.com>
2024-05-25 15:56:10 -0300

    Update env.sh

--- shell/env.sh
+++ shell/env.sh
@@ -6,5 +6,6 @@ export MANPAGER='nvim +Man!'
 export SUDO_PROMPT='Password:'
 export NVIM_LOG_FILE='/dev/null'
 export ZDOTDIR="$HOME/.config/zsh"
+export MESA_SHADER_CACHE_DISABLE="true"
 export XDG_CACHE_HOME="$HOME/.local/cache"
 export FREETYPE_PROPERTIES='cff:no-stem-darkening=0 autofitter:no-stem-darkening=0'
