28c489d7b720469f45f42ce618efec0609f2550f
norphiz <103674674+norphiz@users.noreply.github.com>
2024-05-26 19:43:50 -0300

    Update alias.sh

--- shell/alias.sh
+++ shell/alias.sh
@@ -4,6 +4,7 @@ updt-plugins() {
         "$BASE/vim"
         "$BASE/nvim-autopairs"
         "$BASE/nvim-treesitter"
+        "$HOME/.local/share/wallpaper"
         "$ZDOTDIR/fast-syntax-highlighting"
     )
     for DIR in "${PLUGINS[@]}"
@@ -16,13 +17,21 @@ updt-plugins() {
 
 alias nv='nvim'
 alias cl='clear'
+alias cp='cp -r'
 alias rm='rm -fr'
 alias ls='eza -a'
 alias cat='bat -pp'
+alias pqe='pacman -Qe'
+alias pql='pacman -Ql'
+alias pss='pacman -Ss'
+alias pqi='pacman -Qi'
+alias psi='pacman -Si'
 alias sdls='sudo eza -a'
+alias pqdt='pacman -Qdtq'
 alias inst='sudo pacman -S'
+alias pscc='sudo pacman -Scc'
 alias updt='sudo pacman -Syu'
 alias unst='sudo pacman -Rns'
 alias gc='git clone -q --recursive --depth 1'
-alias sway='clear; sway > .local/share/swaylog 2>&1'
 alias grep='batgrep --context=0 --no-separator --paging=never'
+alias river='clear; pidof -q river && clear || river -no-xwayland'
