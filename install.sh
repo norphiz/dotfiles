eb1bffb950c6408d72b2f4892611be5ca8fc8829
norphiz <103674674+norphiz@users.noreply.github.com>
2024-05-26 19:42:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,22 @@
 
 set -eu
 
+PKG_ARRAY=(
+    base
+    git
+    sudo
+    dhcpcd
+    booster
+    linux
+    intel-ucode
+    linux-firmware
+)
+
 cfdisk
 
 clear
+
+lsblk
 
 read -r -p 'Enter the uefi partition: ' UEFI
 
@@ -24,38 +37,39 @@ swapon "$SWAP"
 
 mkfs.ext4 -L ROOT "$ROOT"
 
+clear
+
 mount "$ROOT" /mnt
 
 mount -m "$BOOT" /mnt/boot
 
 mount -m -o fmask=0077,dmask=0077 "$UEFI" /mnt/efi
 
-reflector -c 'BR,' -p https -f 5 --sort age --save /etc/pacman.d/mirrorlist
+reflector -c ',BR' -p https -f 5 --sort rate --save /etc/pacman.d/mirrorlist
 
-pacstrap -i -K /mnt base iwd intel-ucode git sudo dhcpcd linux{,-firmware}
+printf 'Packages to be installed: %s\n' "${PKG_ARRAY[@]}"
+
+read -r -p 'Enter extra packages to be installed: ' EXTRA_PKGS
+
+PKG_ARRAY+=("$EXTRA_PKGS")
+
+pacstrap -K /mnt "${PKG_ARRAY[@]}"
 
 clear
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-read -r -p 'Enter hostname: ' HNAME
+read -r -p 'Enter hostname: ' HUNAME
 
 sed 's/#en_US.UTF-8/en_US.UTF-8/' -i /mnt/etc/locale.gen
 
-mkdir /mnt/etc/iwd
-
-echo "$HNAME" > /mnt/etc/hostname
+echo "$HUNAME" > /mnt/etc/hostname
 
 echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
 
 echo 'KEYMAP=br-abnt2' > /mnt/etc/vconsole.conf
 
 echo '%wheel ALL=(ALL:ALL) ALL' > /mnt/etc/sudoers.d/sudoers
-
-echo '[Network]
-NameResolvingService=resolvconf
-
-# vi: ft=dosini' > /etc/iwd/main.conf
 
 sed 's/rel/no/' -i /mnt/etc/fstab
 
@@ -73,7 +87,15 @@ passwd "$UNAME"
 
 locale-gen
 
-systemctl -q enable {iwd,dhcpcd,systemd-boot-update}
+if test "$(command -v iwd)"
+then
+    systemctl -q enable iwd
+    mkdir /etc/iwd
+    echo '[Network]
+NameResolvingService=resolvconf' > /etc/iwd/main.conf
+fi
+
+systemctl -q enable {dhcpcd,systemd-boot-update}
 
 bootctl install
 
@@ -87,15 +109,15 @@ initrd intel-ucode.img
 initrd booster-linux.img
 options root=LABEL=ROOT rw quiet loglevel=0' > /boot/loader/entries/arch.conf
 
-git clone -q --depth 1 https://github.com/norphiz/dotfiles.git "/home/$NAME/.config"
+git clone -q --depth 1 https://github.com/norphiz/dotfiles.git "/home/$UNAME/.config"
 
-git clone -q --depth 1 https://github.com/dracula/wallpaper.git "/home/$NAME/.local/share/wallpaper"
+git clone -q --depth 1 https://github.com/dracula/wallpaper.git "/home/$UNAME/.local/share/wallpaper"
 
-git clone -q --depth 1 https://github.com/dracula/vim.git "/home/$NAME/.config/nvim/pack/plugins/start/dracula.vim"
+git clone -q --depth 1 https://github.com/dracula/vim.git "/home/$UNAME/.config/nvim/pack/plugins/start/dracula.vim"
 
-git clone -q --depth 1 https://github.com/windwp/nvim-autopairs.git "/home/$NAME/.config/nvim/pack/plugins/start/nvim-autopairs"
+git clone -q --depth 1 https://github.com/windwp/nvim-autopairs.git "/home/$UNAME/.config/nvim/pack/plugins/start/nvim-autopairs"
 
-git clone -q --depth 1 https://github.com/nvim-treesitter/nvim-treesitter.git "/home/$NAME/.config/nvim/pack/plugins/start/nvim-treesitter"
+git clone -q --depth 1 https://github.com/nvim-treesitter/nvim-treesitter.git "/home/$UNAME/.config/nvim/pack/plugins/start/nvim-treesitter"
 
 echo 'Successfully installed.'
 
