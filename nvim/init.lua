5c8c249eb624f59296ccb6f7cb5678203883c403
norphiz <103674674+norphiz@users.noreply.github.com>
2024-05-26 00:26:55 -0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,22 @@
 vim.o.ls = 0
 vim.o.sw = 4
+
+vim.o.ru = false
+vim.o.swf = false
+vim.o.smd = false
+vim.o.wrap = false
+
 vim.o.nu = true
 vim.o.et = true
 vim.g.ic = true
 vim.o.sc = true
-vim.o.ru = false
 vim.o.scs = true
-vim.o.swf = false
-vim.o.smd = false
+
 vim.o.sdf = "NONE"
-vim.o.wrap = false
 vim.o.fcs = "eob: "
 vim.o.cb = "unnamedplus"
 
 vim.cmd.colo "dracula"
-
-vim.api.nvim_set_keymap("n", ";", ":", {})
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "*",
@@ -28,11 +29,11 @@ if os.getenv "TERM" ~= "linux" then
     vim.o.tgc = true
 end
 
-if os.execute "test -d $HOME/.config/nvim/pack/plugins/start/nvim-autopairs" == 0 then
+if os.execute "test -d $HOME/.config/nvim/pack/plugins/start/nvim-autopairs" then
     require "nvim-autopairs".setup()
 end
 
-if os.execute "test -d $HOME/.config/nvim/pack/plugins/start/nvim-treesitter" == 0 then
+if os.execute "test -d $HOME/.config/nvim/pack/plugins/start/nvim-treesitter" then
     require "nvim-treesitter.configs".setup {
         highlight = {
             enable = true
