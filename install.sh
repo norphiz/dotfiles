59b0b0157843d4c6009b9bfef175829092e29025
norphiz <103674674+norphiz@users.noreply.github.com>
2025-02-13 17:41:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ main()
     initrd booster-linux.img
     options root=LABEL=ROOT rw' > /mnt/boot/loader/entries/arch.conf
     
-    sed -n '88,$p' "$0" > /mnt/chroot.sh
+    sed -n '92,$p' "$0" > /mnt/chroot.sh
 
     arch-chroot /mnt bash chroot.sh
 }
