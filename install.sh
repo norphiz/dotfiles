450f08214d12b0b8fa114f8d09de99edc1ba3cb4
norphiz <103674674+norphiz@users.noreply.github.com>
2025-02-24 13:12:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,9 +79,11 @@ initrd intel-ucode.img
 initrd booster-linux.img
 options root=LABEL=ROOT rw' > /mnt/boot/loader/entries/arch.conf
 
-sed -n '86,$p' "$0" > /mnt/chroot.sh
+sed -n '88,$p' "$0" > /mnt/chroot.sh
 
 arch-chroot /mnt bash chroot.sh
+
+exit
 
 #!/bin/bash
 
