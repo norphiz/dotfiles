df635b0d7b3a2d5b1ce3784a6a4c769ae107b128
norphiz <103674674+norphiz@users.noreply.github.com>
2025-03-05 14:33:15 +0000

    Update env.sh

--- shell/env.sh
+++ shell/env.sh
@@ -7,6 +7,8 @@ export XDG_CACHE_HOME='/tmp'
 export SUDO_PROMPT='Password:'
 export NVIM_LOG_FILE='/dev/null'
 export ZDOTDIR="$HOME/.config/zsh"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
 export XINITRC="$HOME/.config/xorg/xinitrc"
 export XAUTHORITY="$HOME/.local/share/xorg/Xauthority"
 export FREETYPE_PROPERTIES='cff:no-stem-darkening=0 autofitter:no-stem-darkening=0'
