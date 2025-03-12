09bce9706b6400e8549647a40fdc9cb281c4abe0
norphiz <103674674+norphiz@users.noreply.github.com>
2025-03-12 16:40:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -76,7 +76,7 @@ echo 'KEYMAP=br-abnt2' > /mnt/etc/vconsole.conf
 echo '[zram0]
 compression-algorithm = zstd' > /mnt/etc/systemd/zram-generator.conf
 
-sed -n '87,$p' "$0" > /mnt/chroot.sh
+sed -n '85,$p' "$0" > /mnt/chroot.sh
 
 arch-chroot /mnt bash chroot.sh
 
