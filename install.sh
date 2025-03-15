6c9e8127853255232ab99d05bb2bb84b666e9127
norphiz <103674674+norphiz@users.noreply.github.com>
2025-03-15 12:59:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,52 +41,46 @@ echo "KEYMAP=br-abnt2" > /mnt/etc/vconsole.conf
 echo "[zram0]
 compression-algorithm = zstd" > /mnt/etc/systemd/zram-generator.conf
 
-while true; do
+read -r -p 'Are you dual booting? [y/n]: ' ANSWER
 
-    read -r -p "Are you dual booting? [N/y]: " ANSWER
+if test "${ANSWER,,}" = 'y'; then 
+    lsblk
 
-    case "$ANSWER" in
-        [yY])
-            lsblk
+    read -r -p "Enter boot partition: " BOOT
 
-            read -r -p "Enter boot partition: " BOOT
+    mkfs.fat -F 32 "$BOOT" > /dev/null 2>&1
 
-            mkfs.fat -F 32 "$BOOT" > /dev/null 2>&1
+    mount "$BOOT" /mnt/boot
 
-            mount "$BOOT" /mnt/boot
+    mount -m -o fmask=0077,dmask=0077 "$UEFI" /mnt/efi
 
-            mount -m -o fmask=0077,dmask=0077 "$UEFI" /mnt/efi
+    bootctl install --esp-path=/mnt/efi --boot-path=/mnt/boot > /dev/null 2>&1
 
-            bootctl install --esp-path=/mnt/efi --boot-path=/mnt/boot > /dev/null 2>&1
+    echo "editor no
+    timeout 10" > /mnt/efi/loader/loader.conf
 
-            echo "editor no
-            timeout 10" > /mnt/efi/loader/loader.conf
+    echo "title Arch Linux
+    linux vmlinuz-linux
+    initrd intel-ucode.img
+    initrd booster-linux.img
+    options root=UUID=$(blkid "$ROOT" -s UUID -o value) rw quiet" > /mnt/boot/loader/entries/arch.conf
+elif test "${ANSWER,,}" = 'n'; then
+    mkfs.fat -F 32 "$UEFI" > /dev/null 2>&1
 
-            echo "title Arch Linux
-            linux vmlinuz-linux
-            initrd intel-ucode.img
-            initrd booster-linux.img
-            options root=UUID=$(blkid "$ROOT" -s UUID -o value) rw quiet" > /mnt/boot/loader/entries/arch.conf
-            
-            break ;;
-        [nN])
-            mkfs.fat -F 32 "$UEFI" > /dev/null 2>&1
+    mount -o fmask=0077,dmask=0077 "$UEFI" /mnt/boot
 
-            mount -o fmask=0077,dmask=0077 "$UEFI" /mnt/boot
+    bootctl install --esp-path=/mnt/boot > /dev/null 2>&1
 
-            bootctl install --esp-path=/mnt/boot > /dev/null 2>&1
+    ln -s /usr/share/zoneinfo/America/Fortaleza /mnt/etc/localtime
 
-            ln -s /usr/share/zoneinfo/America/Fortaleza /mnt/etc/localtime
-
-            echo "title Arch Linux
-            linux vmlinuz-linux
-            initrd intel-ucode.img
-            initrd booster-linux.img
-            options root=UUID=$(blkid "$ROOT" -s UUID -o value) rw quiet" > /mnt/boot/loader/entries/arch.conf
-            
-            break ;;
-    esac
-done
+    echo "title Arch Linux
+    linux vmlinuz-linux
+    initrd intel-ucode.img
+    initrd booster-linux.img
+    options root=UUID=$(blkid "$ROOT" -s UUID -o value) rw quiet" > /mnt/boot/loader/entries/arch.conf
+else
+    return 1
+fi
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
@@ -113,7 +107,6 @@ echo "$NAME ALL=(ALL:ALL) ALL" > /mnt/etc/sudoers.d/sudoers
 echo 'export ZDOTDIR="$HOME/.config/zsh"' > /mnt/etc/zsh/zshenv
 
 if test -e /mnt/usr/bin/iwctl; then
-
     pacstrap /mnt wireless-regdb
 
     arch-chroot /mnt systemctl -q enable iwd
