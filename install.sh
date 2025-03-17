8a07e2903514638999bc9b155d87b81029ddf478
norphiz <103674674+norphiz@users.noreply.github.com>
2025-03-16 22:35:35 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,6 +26,10 @@ read -r -p "Are you dual booting? [y/n]: " DUALBOOT
 
 read -r -p "Install linux-firmware? [y/n]: " FIRMWARE
 
+mkfs.ext4 -q "${PARTITIONS[1]}"
+
+mount "${PARTITIONS[1]}" /mnt
+
 if test "${FIRMWARE,,}" = "y"; then
     PKGS+=("iwd" "wireless-regdb" "linux-firmware")
 fi
@@ -34,10 +38,6 @@ if test "${DUALBOOT,,}" = "y"; then
     read -r -p "Enter boot partition: " BOOT
 
     mkfs.fat -F 32 "$BOOT" > /dev/null
-
-    mkfs.ext4 -q "${PARTITIONS[1]}"
-
-    mount "${PARTITIONS[1]}" /mnt
 
     mount -m -o fmask=0077,dmask=0077 "${PARTITIONS[0]}" /mnt/efi
 
@@ -48,18 +48,8 @@ if test "${DUALBOOT,,}" = "y"; then
     bootctl install --esp-path=/mnt/efi --boot-path=/mnt/boot > /dev/null 2>&1
 
     echo "timeout 10" > /mnt/efi/loader/loader.conf
-
-    echo "title Arch Linux
-linux vmlinuz-linux
-initrd intel-ucode.img
-initrd booster-linux.img
-options root=UUID=$(blkid "${PARTITIONS[1]}" -s UUID -o value) rw quiet" > /mnt/boot/loader/entries/arch.conf
 elif test "${DUALBOOT,,}" = "n"; then
     mkfs.fat -F 32 "${PARTITIONS[0]}" > /dev/null
-
-    mkfs.ext4 -q "${PARTITIONS[1]}"
-
-    mount "${PARTITIONS[1]}" /mnt
 
     mount -m -o fmask=0077,dmask=0077 "${PARTITIONS[0]}" /mnt/boot
     
@@ -68,14 +58,18 @@ elif test "${DUALBOOT,,}" = "n"; then
     ln -s /usr/share/zoneinfo/America/Fortaleza /mnt/etc/localtime
 
     bootctl install --esp-path=/mnt/boot > /dev/null 2>&1
-    
-    echo "title Arch Linux
-linux vmlinuz-linux
-initrd intel-ucode.img
-initrd booster-linux.img
-options root=UUID=$(blkid "${PARTITIONS[1]}" -s UUID -o value) rw quiet" > /mnt/boot/loader/entries/arch.conf
 else
     exit 1
+fi
+
+if test "${FIRMWARE,,}" = "y"; then
+    arch-chroot /mnt systemctl -q enable iwd
+
+    mkdir /mnt/etc/iwd
+
+    echo "[General]
+AddressRandomization=once
+AddressRandomizationRange=full" > /mnt/etc/iwd/main.conf
 fi
 
 read -r -p "Enter username: " NAME
@@ -90,16 +84,6 @@ arch-chroot /mnt systemctl -q enable dhcpcd systemd-boot-update
 
 arch-chroot /mnt systemctl -q disable systemd-userdbd.socket
 
-if test "${FIRMWARE,,}" = "y"; then
-    arch-chroot /mnt systemctl -q enable iwd
-
-    mkdir /mnt/etc/iwd
-
-    echo "[General]
-AddressRandomization=once
-AddressRandomizationRange=full" > /mnt/etc/iwd/main.conf
-fi
-
 echo "arch" > /mnt/etc/hostname
 
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
@@ -112,5 +96,11 @@ echo 'export ZDOTDIR="$HOME/.config/zsh"' > /mnt/etc/zsh/zshenv
 
 echo "[zram0]
 compression-algorithm = zstd" > /mnt/etc/systemd/zram-generator.conf
+
+echo "title Arch Linux
+linux vmlinuz-linux
+initrd intel-ucode.img
+initrd booster-linux.img
+options root=UUID=$(blkid "${PARTITIONS[1]}" -s UUID -o value) rw quiet" > /mnt/boot/loader/entries/arch.conf
 
 umount -R /mnt
