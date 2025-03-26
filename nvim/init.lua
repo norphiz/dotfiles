ff27950c98f4042cacf0eb39996e36e82698a0c2
norphiz <103674674+norphiz@users.noreply.github.com>
2025-03-26 09:41:15 -0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,11 +24,20 @@ vim.g.netrw_winsize = 20
 vim.g.netrw_dirhistmax = 0
 
 vim.keymap.set("n", ";", ":", {})
-vim.keymap.set("n", "<Leader>s", ":x<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>w", ":w<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>q", ":q!<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>c", ":q<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>s", ":so<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>q", ":qa<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>e", ":Lex<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>t", ":tabe<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>e", ":Lexplore<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>w", ":sil :w<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>h", ":winc h<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>j", ":winc j<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>k", ":winc k<CR>", { silent = true })
+vim.keymap.set("n", "<Leader>l", ":winc l<CR>", { silent = true })
+vim.keymap.set("n", "<Leader><Up>", ":winc k<CR>", { silent = true })
+vim.keymap.set("n", "<Leader><Left>", ":winc h<CR>", { silent = true })
+vim.keymap.set("n", "<Leader><Down>", ":winc j<CR>", { silent = true })
+vim.keymap.set("n", "<Leader><Right>", ":winc l<CR>", { silent = true })
 
 if os.getenv("TERM") ~= "linux" then
     vim.o.tgc = true
