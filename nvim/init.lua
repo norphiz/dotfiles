20c3b38ddde04752e7f44bc450dc0d5ff5b52d1d
norphiz <103674674+norphiz@users.noreply.github.com>
2025-03-27 12:40:26 -0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,27 +18,26 @@ vim.o.sdf = "NONE"
 vim.o.fcs = "eob: "
 vim.o.cb = "unnamedplus"
 
-vim.g.mapleader = " "
 vim.g.netrw_banner = 0
 vim.g.netrw_cursor = 0
 vim.g.netrw_winsize = 20
 vim.g.netrw_dirhistmax = 0
 
 vim.keymap.set("n", ";", ":", {})
-vim.keymap.set("n", "<Leader>c", ":q<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>s", ":so<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>q", ":qa!<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>e", ":Lex<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>t", ":tabe<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>w", ":sil w<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>h", ":winc h<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>j", ":winc j<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>k", ":winc k<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>l", ":winc l<CR>", { silent = true })
-vim.keymap.set("n", "<Leader><Up>", ":winc k<CR>", { silent = true })
-vim.keymap.set("n", "<Leader><Left>", ":winc h<CR>", { silent = true })
-vim.keymap.set("n", "<Leader><Down>", ":winc j<CR>", { silent = true })
-vim.keymap.set("n", "<Leader><Right>", ":winc l<CR>", { silent = true })
+vim.keymap.set("n", "<M-c>", ":q<CR>", { silent = true })
+vim.keymap.set("n", "<M-s>", ":so<CR>", { silent = true })
+vim.keymap.set("n", "<M-q>", ":qa!<CR>", { silent = true })
+vim.keymap.set("n", "<M-e>", ":Lex<CR>", { silent = true })
+vim.keymap.set("n", "<M-t>", ":tabe<CR>", { silent = true })
+vim.keymap.set("n", "<M-w>", ":sil w<CR>", { silent = true })
+vim.keymap.set("n", "<M-h>", ":winc h<CR>", { silent = true })
+vim.keymap.set("n", "<M-j>", ":winc j<CR>", { silent = true })
+vim.keymap.set("n", "<M-k>", ":winc k<CR>", { silent = true })
+vim.keymap.set("n", "<M-l>", ":winc l<CR>", { silent = true })
+vim.keymap.set("n", "<M-Up>", ":winc k<CR>", { silent = true })
+vim.keymap.set("n", "<M-Left>", ":winc h<CR>", { silent = true })
+vim.keymap.set("n", "<M-Down>", ":winc j<CR>", { silent = true })
+vim.keymap.set("n", "<M-Right>", ":winc l<CR>", { silent = true })
 
 if os.getenv("TERM") ~= "linux" then
     vim.o.tgc = true
