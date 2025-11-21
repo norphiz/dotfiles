98ebb39ea5a4fc51cb34f8236498b1803777262b
norphiz <103674674+norphiz@users.noreply.github.com>
2025-11-21 16:01:14 -0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,7 +18,7 @@ vim.o.cb = "unnamedplus"
 
 vim.g.netrw_banner = 0
 vim.g.netrw_cursor = 0
-vim.g.netrw_winsize = 20
+vim.g.netrw_winsize = 12
 vim.g.netrw_dirhistmax = 0
 
 vim.keymap.set("n", ";", ":", {})
